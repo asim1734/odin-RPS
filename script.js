579d491c19ec19d9635193a7f641c6c325373764
asim1734 <asim.rupani@gmail.com>
2024-10-04 21:48:39 +0530

    Add function to display result,overhaul the design
    Use AI generated images to make cooler buttons and background.
    Create and display the choosen elements in the center

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const playerChoice= document.querySelectorAll(".choice");
 let humanScore = 0;
 let computerScore = 0;
-const moves = ["rock","paper","scissors"];
+const moves = ["rock","paper","scissor"];
 
 playerChoice.forEach(choice =>{
     choice.addEventListener('click' , playRound);
@@ -19,7 +19,7 @@ function playRound(event){
     const humanChoice = getHumanChoice(event);
     const computerChoice = getComputerChoice();
     const verdict = determineWinner(humanChoice , computerChoice);
-    displayResult(verdict);
+    displayResult(verdict, humanChoice , computerChoice);
 }
 
 function determineWinner(humanChoice , computerChoice){
@@ -29,6 +29,26 @@ function determineWinner(humanChoice , computerChoice){
     if (decider == 0) return 0;
     else if (decider == 1) return -1;
     else return 1
+}
+
+function displayResult(verdict, humanChoice , computerChoice){
+    const versusDiv = document.querySelector(".versus");
+
+    const humanChoiceImage = document.createElement("img");
+    const computerChoiceImage = document.createElement("img");
+
+    humanChoiceImage.src = `${humanChoice}.jpg`;
+    humanChoiceImage.style.width = "15%";
+    versusDiv.appendChild(humanChoiceImage);
+
+    const VSText = document.createElement("p");
+    VSText.textContent = "VS."
+    VSText.style.cssText = "display: inline"
+    versusDiv.appendChild(VSText);
+
+    computerChoiceImage.src = `${computerChoice}.jpg`;
+    computerChoiceImage.style.width = "15%";
+    versusDiv.appendChild(computerChoiceImage);
 }
 
 function playGame(){
